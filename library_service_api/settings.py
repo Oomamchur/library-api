e98c7d740576aa14f06fcc75354121b422487684
Oleksandr Mamchur <irex1988@gmail.com>
2023-07-21 18:02:37 +0300

    add new app user

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "library",
+    "user",
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
