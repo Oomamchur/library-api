e16da80c2599369fa69cb41d9e8f8965007638c0
Oleksandr Mamchur <irex1988@gmail.com>
2023-07-25 19:36:03 +0300

    added custom permissions to Book endpoints

--- library/views.py
+++ library/views.py
@@ -1,13 +1,14 @@
 from rest_framework import viewsets
 
 from library.models import Book
+from library.permissions import IsAdminOrReadOnly
 from library.serializers import BookSerializer, BookListSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    # permission_classes = ()
+    permission_classes = (IsAdminOrReadOnly,)
 
     def get_serializer_class(self):
         if self.action == "list":
