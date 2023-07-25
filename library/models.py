dde0f302d0d0adee8d7d0e74efd07603ce01c8e7
Oleksandr Mamchur <irex1988@gmail.com>
2023-07-25 20:19:24 +0300

    added blank=True to actual_return_date

--- library/models.py
+++ library/models.py
@@ -35,7 +35,7 @@ class Book(models.Model):
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
