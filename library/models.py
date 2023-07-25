afd9b5c11a1da964217f941950a3cf187a68e65b
Oleksandr Mamchur <irex1988@gmail.com>
2023-07-25 17:29:35 +0300

    implemented Book and Borrow models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    inventory = models.IntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        ordering = ["title", "author"]
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["borrow_date"]
+
+    def __str__(self) -> str:
+        return f"Book: {self.book.title}, return date: {self.expected_return_date}"
